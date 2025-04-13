420a0f0caeddbb6250125a9263a197faf8524e11
Jonathan Alonso <jonathanalonsom@outlook.com>
2025-04-13 09:31:27 +0100

    correcao do webmananger

--- selenium_helper.py
+++ selenium_helper.py
@@ -7,6 +7,9 @@ import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
 
 def iniciar_navegador():
@@ -17,7 +20,7 @@ def iniciar_navegador():
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
 
-    navegador = webdriver.Chrome(options=options)  #options=options 
+    navegador = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     navegador.maximize_window()
     return navegador
 
